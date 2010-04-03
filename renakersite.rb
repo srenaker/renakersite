2ae2e761c9e9b7a57f07a9830403c9193034eee7
srenaker <renaker@yahoo.com>
2010-04-02 22:19:19 -0700

    starting slideshow

--- renakersite.rb
+++ renakersite.rb
@@ -21,7 +21,46 @@ get '/gallery' do
   erb :gallery
 end
 
+get '/slideshow/1' do
+  erb :slide1
+end
 
+get '/slideshow/2' do
+  erb :slide2
+end
 
+get '/slideshow/3' do
+  erb :slide3
+end
 
+get '/slideshow/4' do
+  erb :slide4
+end
 
+get '/slideshow/5' do
+  erb :slide5
+end
+
+get '/slideshow/6' do
+  erb :slide6
+end
+
+get '/slideshow/7' do
+  erb :slide7
+end
+
+get '/slideshow/8' do
+  erb :slide8
+end
+
+get '/slideshow/9' do
+  erb :slide9
+end
+
+get '/slideshow/10' do
+  erb :slide102
+end
+
+get '/slideshow/11' do
+  erb :slide11
+end
